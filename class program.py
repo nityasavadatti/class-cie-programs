ca662a6a4123607ef45638aa6e49dd94e7caf5a5
nityasavadatti <nityasavadatti39@gmail.com>
2025-11-27 14:21:34 +0530

    Update class program.py

--- class program.py
+++ class program.py
@@ -1,35 +1,41 @@
 import sys
-if len (sys.argv) == 6:
- script_name =sys.argv[0]
- m1=sys.argv[1]
- m2=sys.argv[2]
- m3=sys.argv[3]
- m4=sys.argv[4]
- m5=sys.argv[5]
+
+if len(sys.argv) == 6:
+    script_name = sys.argv[0]
+    m1 = float(sys.argv[1])
+    m2 = float(sys.argv[2])
+    m3 = float(sys.argv[3])
+    m4 = float(sys.argv[4])
+    m5 = float(sys.argv[5])
 else:
-  script_name =sys.argv[0]
-  m1=60
-  m1=80
-  m1=94
-  m1=82
-  m1=78
-avg=(m1+m2+m3+m4+m5)/5
-if(avg>90)
-grade='A+'
-elif(avg>80)
-grade='A'
-elif(avg>70)
-grade='B'
-elif(avg>60)
-grade='C'
-elif(avg>50)
-grade='fail'
+    script_name = sys.argv[0]
+    # Default marks
+    m1 = 60
+    m2 = 80
+    m3 = 94
+    m4 = 82
+    m5 = 78
 
-print("marks 1",m1)
-print("marks 2",m2)
-print("marks 3",m3)
-print("marks 4",m4)
-print("marks 5",m5)
-print("average",avg)
-print("grade",grade)
+avg = (m1 + m2 + m3 + m4 + m5) / 5
 
+# Grade calculation
+if avg > 90:
+    grade = "A+"
+elif avg > 80:
+    grade = "A"
+elif avg > 70:
+    grade = "B"
+elif avg > 60:
+    grade = "C"
+elif avg > 50:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("Marks 1:", m1)
+print("Marks 2:", m2)
+print("Marks 3:", m3)
+print("Marks 4:", m4)
+print("Marks 5:", m5)
+print("Average:", avg)
+print("Grade:", grade)
